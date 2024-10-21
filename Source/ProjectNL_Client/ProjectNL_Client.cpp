07a8c6dd15bbb08d60911bdf9626d75fc7027347
GoldFrosch <goldfrosch@daum.net>
2024-10-21 17:19:13 +0900

    initial project

---
+++ Source/ProjectNL_Client/ProjectNL_Client.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectNL_Client.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectNL_Client, "ProjectNL_Client" );
