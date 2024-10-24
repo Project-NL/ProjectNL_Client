f50647fd763272cc784c5b0873efede90c583e35
GoldFrosch <goldfrosch@daum.net>
2024-10-24 14:38:24 +0900

    feat: attribute, gas 초기 아키텍쳐 연동

---
+++ Source/ProjectNL/GAS/NLAbilitySystemComponent.cpp
@@ -0,0 +1,63 @@
+﻿#include "NLAbilitySystemComponent.h"
+
+#include "NLAbilitySystemInitializationData.h"
+
+
+UNLAbilitySystemComponent::UNLAbilitySystemComponent()
+{
+	ReplicationMode = EGameplayEffectReplicationMode::Mixed;
+	SetIsReplicatedByDefault(true);
+}
+
+void UNLAbilitySystemComponent::InitializeAbilitySystem(
+	const FNLAbilitySystemInitializationData& InitData)
+{
+	if (GetIsInitialized())
+	{
+		return;
+	}
+
+	if (GetOwnerRole() == ROLE_Authority)
+	{
+		if (IsValid(InitData.FirstNameAbility))
+		{
+			UGameplayAbility* Ability = InitData.FirstNameAbility->GetDefaultObject<
+				UGameplayAbility>();
+			GiveAbility(FGameplayAbilitySpec(
+				Ability, Ability->GetAbilityLevel(), -1, this));
+		}
+
+		if (IsValid(InitData.SecondNameAbility))
+		{
+			UGameplayAbility* Ability = InitData.SecondNameAbility->GetDefaultObject<
+				UGameplayAbility>();
+			GiveAbility(FGameplayAbilitySpec(
+				Ability, Ability->GetAbilityLevel(), -1, this));
+		}
+
+		if (IsValid(InitData.ThirdNameAbility))
+		{
+			UGameplayAbility* Ability = InitData.ThirdNameAbility->GetDefaultObject<
+				UGameplayAbility>();
+			GiveAbility(FGameplayAbilitySpec(
+				Ability, Ability->GetAbilityLevel(), -1, this));
+		}
+
+		if (!InitData.GameplayEffects.IsEmpty())
+		{
+			for (TSubclassOf<UGameplayEffect> Effect : InitData.GameplayEffects)
+			{
+				ApplyGameplayEffectToSelf(Effect.GetDefaultObject(), 1
+																	, MakeEffectContext());
+			}
+		}
+	}
+
+	if (!InitData.GameplayTags.IsEmpty())
+	{
+		AddLooseGameplayTags(InitData.GameplayTags);
+	}
+
+
+	SetIsInitialized(true);
+}
---
+++ Source/ProjectNL/GAS/NLAbilitySystemInitializationData.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "NLAbilitySystemInitializationData.generated.h"
+
+class UBasicAttributeSet;
+class UGameplayAbility;
+class UBaseInputTriggerAbility;
+class UAttributeSet;
+class UGameplayEffect;
+
+USTRUCT(BlueprintType)
+struct FNLAbilitySystemInitializationData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TArray<TSubclassOf<UBasicAttributeSet>> AttributeSets;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TArray<TSubclassOf<UBaseInputTriggerAbility>> GameplayAbilities;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<UGameplayAbility> FirstNameAbility;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<UGameplayAbility> SecondNameAbility;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<UGameplayAbility> ThirdNameAbility;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TArray<TSubclassOf<UGameplayEffect>> GameplayEffects;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FGameplayTagContainer GameplayTags;
+};
