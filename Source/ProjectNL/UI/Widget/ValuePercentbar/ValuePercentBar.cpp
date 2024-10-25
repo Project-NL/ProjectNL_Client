e38cbda741542062256dc3b9a167aa9f8e8b797f
GoldFrosch <goldfrosch@daum.net>
2024-10-26 00:24:22 +0900

    feat: value percent bar umg 추가

---
+++ Source/ProjectNL/UI/Widget/ValuePercentbar/ValuePercentBar.cpp
@@ -0,0 +1,58 @@
+﻿#include "ValuePercentBar.h"
+#include "Components/ProgressBar.h"
+
+void UValuePercentBar::InitializePercent(const float CurrentValue, const float MaxValue)
+{
+	PercentNum.Key = CurrentValue;
+	PercentNum.Value = MaxValue;
+
+	// const float Percent = PercentNum.Key / PercentNum.Value;
+	ViewPercentBar->SetPercent(1.f);
+	DelayViewPercentBar->SetPercent(1.f);
+}
+
+void UValuePercentBar::SetCurrentValue(const float NewValue)
+{
+	// 새로 들어가는 값이 기존의 값보다 작은 경우 점차 줄어드는 애니메이션 수행
+	if (NewValue < PercentNum.Key)
+	{
+		const float PrevCurrentValue = PercentNum.Key;
+		PercentNum.Key = NewValue;
+		const float NewPercent = PercentNum.Key / PercentNum.Value;
+		ViewPercentBar->SetPercent(NewPercent);
+		DecreaseDelayPercentBar(PrevCurrentValue, NewPercent, true);
+	}
+	else
+	{
+		// 아니라면 새로운 값 적용 및 Percent 전부 즉시 적용
+		PercentNum.Key = NewValue;
+		const float NewPercent = PercentNum.Key / PercentNum.Value;
+	
+		ViewPercentBar->SetPercent(NewPercent);
+		DelayViewPercentBar->SetPercent(NewPercent);
+	}
+}
+
+void UValuePercentBar::SetMaxValue(const float NewValue)
+{
+	// 아니라면 새로운 값 적용 및 Percent 전부 즉시 적용
+	PercentNum.Value = NewValue;
+	const float NewPercent = PercentNum.Key / PercentNum.Value;
+	
+	ViewPercentBar->SetPercent(NewPercent);
+	DelayViewPercentBar->SetPercent(NewPercent);
+}
+
+void UValuePercentBar::DecreaseDelayPercentBar(const float CurrentPercent, const float FinalPercent, const bool IsStart)
+{
+	if (CurrentPercent - 0.1 > FinalPercent)
+	{
+		GetWorld()->GetTimerManager().SetTimer(SetDelayViewPercentTimerHandle, FTimerDelegate::CreateLambda([&]
+		{
+			DecreaseDelayPercentBar(CurrentPercent - 0.1, FinalPercent, false);
+		}), IsStart ? DelayedTime : PercentDecreaseTime, false);
+	} else
+	{
+		GetWorld()->GetTimerManager().ClearTimer(SetDelayViewPercentTimerHandle);
+	}
+}
---
+++ Source/ProjectNL/UI/Widget/ValuePercentbar/ValuePercentBar.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ValuePercentBar.generated.h"
+
+class UProgressBar;
+
+UCLASS()
+class PROJECTNL_API UValuePercentBar : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void InitializePercent(const float CurrentValue, const float MaxValue);
+
+	void SetCurrentValue(const float NewValue);
+
+	void SetMaxValue(const float NewValue);
+protected:
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UProgressBar> ViewPercentBar;
+	
+	UPROPERTY(EditDefaultsOnly, meta = (BindWidget))
+	TObjectPtr<UProgressBar> DelayViewPercentBar;
+
+private:
+	// N초 뒤 실행할 변수 값
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	float DelayedTime = 2;
+
+	// 줄어드는 시간 단위
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	float PercentDecreaseTime = 0.02;
+	
+	FTimerHandle SetDelayViewPercentTimerHandle;
+	
+	void DecreaseDelayPercentBar(const float CurrentPercent, const float FinalPercent, const bool IsStart);
+	
+	// 0번째 현재 값, 1번째 최대 값
+	TPair<float, float> PercentNum;
+};
