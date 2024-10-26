e4a3d5e62bcd4aded4161331ba38596fdacff52a
GoldFrosch <goldfrosch@daum.net>
2024-10-26 22:17:54 +0900

    feat: 장비 관리 액터 컴포넌트 추가
    1. feat: 장비 관리 액터 컴포넌트 추가
    2. feat: 전투 애니메이션 관련 데이터 테이블 추가

---
+++ Source/ProjectNL/Component/EquipComponent/EquipComponent.cpp
@@ -0,0 +1,22 @@
+﻿#include "EquipComponent.h"
+#include "ProjectNL/Weapon/BaseWeapon.h"
+
+UEquipComponent::UEquipComponent()
+{
+	// 플레이어 에게는 테스트 코드가 될 것 (실제로 Class를 주입해서 하는 것이 아닌 인벤토리를 통하기 때문)
+	// 다만 플레이어가 아닌 다른 엔티티 기준으로는 테스트 코드가 아닌 컴파일 과정에서 바로 Weapon 객체를 주입하게 됨
+	MainWeapon = MainWeaponClass.GetDefaultObject();
+	SubWeapon = SubWeaponClass.GetDefaultObject();
+}
+
+
+// Called when the game starts
+void UEquipComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UEquipComponent::UpdateEquipWeaponAnimationData()
+{
+	
+}
---
+++ Source/ProjectNL/Component/EquipComponent/EquipComponent.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EquipComponent.generated.h"
+
+
+class ABaseWeapon;
+
+// TODO: 해당 컴포넌트의 이름에 대한 고민이 더 필요하다.
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTNL_API UEquipComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEquipComponent();
+
+	void UpdateEquipWeaponAnimationData();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(Category="Property", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	bool IsFirstEquipWeapon;
+	
+	UPROPERTY(Category="Property|Weapon", EditDefaultsOnly, meta = (AllowPrivateAccess = "true"))
+	FDataTableRowHandle CombatAnimData;
+	
+	UPROPERTY(Category="Property|Weapon", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<ABaseWeapon> MainWeaponClass;
+
+	UPROPERTY(Category="Property|Weapon", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<ABaseWeapon> SubWeaponClass;
+
+	TObjectPtr<ABaseWeapon> MainWeapon;
+	
+	TObjectPtr<ABaseWeapon> SubWeapon;
+};
