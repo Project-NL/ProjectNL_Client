69b14670c356e60f63ed73ab82edfa473dcee9f6
GoldFrosch <goldfrosch@daum.net>
2024-10-30 22:23:22 +0900

    feat: GameplayTag 연동

---
+++ Source/ProjectNL/Helper/GameplayTagHelper.cpp
@@ -0,0 +1,11 @@
+﻿#include "GameplayTagHelper.h"
+#include "NativeGameplayTags.h"
+
+namespace NlGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_Combat, "Ability", "Ability 관련 최상위 트리 태그");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Category, "Category", "특정 Actor의 카테고리 관련 최상위 트리 태그");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Category_Entity, "Category.Entity", "특정 Actor의 카테고리가 엔티티(살아있는 생물체)인 경우");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State, "State", "특정 액터의 상태에 대한 최상위 트리 태그");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Idle, "State.Idle", "특정 액터의 상태가 어떠한 동작하지 않는 대기 상태인 경우 태그");
+}
---
+++ Source/ProjectNL/Helper/GameplayTagHelper.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "NativeGameplayTags.h"
+
+struct FGameplayTag;
+
+namespace NlGameplayTags
+{
+	PROJECTNL_API FGameplayTag FindTagByString(const FString& TagString
+																						, bool bMatchPartialString = false);
+
+	// Ability 관련 최상위 트리 태그
+	PROJECTNL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability)
+	// 특정 Actor의 카테고리 관련 최상위 트리 태그 (ex. 플레이어, 엔티티, 오브젝트 등)
+	PROJECTNL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Category)
+	PROJECTNL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Category_Entity)
+	// 특정 액터의 상태에 대한 최상위 트리 태그
+	PROJECTNL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State)
+	PROJECTNL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Idle)
+}
