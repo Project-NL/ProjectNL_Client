cd94b73d8e41c657e919c319223b24ffb7edb0f9
GoldFrosch <goldfrosch@daum.net>
2024-11-20 14:14:43 +0900

    refactor: 방어 로직 올바른 Delegate 사용 변경
    1. refactor: BlockStartNotify 에 Delegate 제거
    2. refactor: ASC에는 데미지 시작 전 Delegate 추가
    3.

--- Source/ProjectNL/GAS/Ability/Active/Default/Guard/GA_Guard.cpp
+++ Source/ProjectNL/GAS/Ability/Active/Default/Guard/GA_Guard.cpp
@@ -1,9 +1,7 @@
 ﻿#include "GA_Guard.h"
 
-#include "AnimNotify/BlockStartNotify.h"
 #include "ProjectNL/Character/Player/PlayerCharacter.h"
 #include "ProjectNL/Component/EquipComponent/EquipComponent.h"
-#include "ProjectNL/GAS/Ability/Utility/AT_PutDamageWithEvent.h"
 #include "ProjectNL/GAS/Ability/Utility/PlayMontageWithEvent.h"
 #include "ProjectNL/GAS/Attribute/PlayerAttributeSet.h"
 #include "ProjectNL/Helper/GameplayTagHelper.h"
@@ -18,11 +16,6 @@ void UGA_Guard::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	GetAbilitySystemComponentFromActorInfo()->SetLooseGameplayTagCount(NlGameplayTags::Status_Guard, 1);
-
-	DamageWithEventTask = UAT_PutDamageWithEvent::InitialEvent(this);
-	DamageWithEventTask->OnDamage.AddDynamic(this, &ThisClass::GuardDamage);
-	DamageWithEventTask->ReadyForActivation();
-
 	
 	if (ABaseCharacter* CharacterInfo = Cast<ABaseCharacter>(GetAvatarActorFromActorInfo()))
 	{
@@ -32,57 +25,43 @@ void UGA_Guard::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 		// 이 액션이 중단될 수 있으니 주의하면 좋다.
 		// TODO: 애니메이션 Binding에 대해 AvatarSet에서 진행해 EquipComponent에 주입하는 것도 좋아보임
 		SetCurrentMontage(EquipComponent->GetBlockAnim());
-		for (FAnimNotifyEvent NotifyEvent : GetCurrentMontage()->Notifies)
-		{
-			if (UBlockStartNotify* BSNotify = Cast<UBlockStartNotify>(NotifyEvent.Notify))
-         	{
-         		BSNotify->OnNotified.AddDynamic(this, &ThisClass::OnBlockStart);
-				BlockStartNotify = BSNotify;
-         	}
-		}
 		
 		AnimationTask = UPlayMontageWithEvent::InitialEvent(this, NAME_None,
 			GetCurrentMontage(), FGameplayTagContainer());
 		AnimationTask->OnCancelled.AddDynamic(this, &ThisClass::UGA_Guard::EndBlock);
 		AnimationTask->OnCompleted.AddDynamic(this, &ThisClass::UGA_Guard::EndBlock);
+
+		if (UNLAbilitySystemComponent* ASC = Cast<UNLAbilitySystemComponent>(GetAbilitySystemComponentFromActorInfo()))
+		{
+			ASC->OnDamageStartedNotified.AddDynamic(this, &ThisClass::StartBlock);
+		}
 	}
 }
 
-void UGA_Guard::OnBlockStart()
+void UGA_Guard::StartBlock(float Damage)
 {
-	FStateHelper::ChangePlayerState(GetAbilitySystemComponentFromActorInfo(), NlGameplayTags::Status_Block, NlGameplayTags::Status_Guard);
+	GetAbilitySystemComponentFromActorInfo()->SetLooseGameplayTagCount(NlGameplayTags::Status_Block, 1);
+	AnimationTask->ReadyForActivation();
 }
-
 
 void UGA_Guard::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
 {
-	GetAbilitySystemComponentFromActorInfo()->SetLooseGameplayTagCount(NlGameplayTags::Status_Block, 0);
-	GetAbilitySystemComponentFromActorInfo()->SetLooseGameplayTagCount(NlGameplayTags::Status_Guard, 0);
 	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
 
 void UGA_Guard::EndBlock(FGameplayTag EventTag, FGameplayEventData EventData)
 {
-	check(BlockStartNotify)
-	BlockStartNotify->OnNotified.RemoveAll(this);
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
 
-
-void UGA_Guard::GuardDamage(float Damage)
+void UGA_Guard::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	// 가드 상태일 때 실행하는 함수
-	if (AnimationTask && GetAbilitySystemComponentFromActorInfo()->HasMatchingGameplayTag(NlGameplayTags::Status_Guard))
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	if (UNLAbilitySystemComponent* ASC = Cast<UNLAbilitySystemComponent>(GetAbilitySystemComponentFromActorInfo()))
 	{
-		FStateHelper::ChangePlayerState(GetAbilitySystemComponentFromActorInfo(), NlGameplayTags::Status_Guard, NlGameplayTags::Status_Block);
-		AnimationTask->ReadyForActivation();
-		return;
-	}
-
-	// 그 외에 즉 Blocking 상태인 경우 실행하는 함수
-	if (GetAbilitySystemComponentFromActorInfo()->HasMatchingGameplayTag(NlGameplayTags::Status_Block))
-	{
-		GetAbilitySystemComponentFromActorInfo()->RemoveLooseGameplayTag(NlGameplayTags::Status_Block);
-		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+		ASC->SetLooseGameplayTagCount(NlGameplayTags::Status_Block, 0);
+		ASC->SetLooseGameplayTagCount(NlGameplayTags::Status_Guard, 0);
+		ASC->OnDamageStartedNotified.RemoveDynamic(this, &ThisClass::StartBlock);
 	}
 }
+
