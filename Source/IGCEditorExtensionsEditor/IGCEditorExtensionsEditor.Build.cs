7eb2869186c43a1beec7426341dbba85f4e969aa
changgeunjoe <hoi000115@gmail.com>
2025-01-05 14:46:37 +0900

    feat : IGCEditorExtensionsEditor.Build.cs widget관련 모든 종속성 추가
    위 작업으로 IGCEditorExtensionsEditor에서 에디터 관련 함수들 사용가능

--- Source/IGCEditorExtensionsEditor/IGCEditorExtensionsEditor.Build.cs
+++ Source/IGCEditorExtensionsEditor/IGCEditorExtensionsEditor.Build.cs
@@ -7,7 +7,14 @@ public class IGCEditorExtensionsEditor : ModuleRules
 	public IGCEditorExtensionsEditor(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+		PublicIncludePathModuleNames.AddRange(
+			new string[] {
+				"Engine",
+				"SkeletonEditor",
+				"AnimationEditor",
+				"AdvancedPreviewScene",
+			}
+		);
 		PublicDependencyModuleNames.AddRange(
 			new string[] 
 			{ 
@@ -22,14 +29,87 @@ public class IGCEditorExtensionsEditor : ModuleRules
                 "AdvancedPreviewScene",
                 "EditorStyle",
                 "InputCore", 
-                "Persona"
+                "Persona",
+                "AnimGraphRuntime" ,
+                "AnimationEditMode",
             }
 		);
 
-		PrivateDependencyModuleNames.AddRange(new string[] 
-		{   
-		
-			
-		});
-	}
+        PrivateIncludePathModuleNames.AddRange(
+                new string[] {
+                "AssetRegistry",
+                "MainFrame",
+                "DesktopPlatform",
+                "AssetTools",
+                "MeshReductionInterface",
+                "SequenceRecorder",
+                "AnimationBlueprintEditor",
+                "EditorInteractiveToolsFramework",
+                }
+            );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[] {
+                "AppFramework",
+                "AnimationModifiers",
+                "AnimationBlueprintLibrary",
+                "AudioEditor",
+                "Core",
+                "CoreUObject",
+                "ApplicationCore",
+                "Slate",
+                "SlateCore",
+                "ContentBrowserData",
+                "EditorStyle",
+                "Engine",
+                "EditorFramework",
+                "UnrealEd",
+                "GraphEditor",
+                "InputCore",
+                "Kismet",
+                "KismetWidgets",
+                "AnimGraph",
+                "PropertyEditor",
+                "EditorWidgets",
+                "DesktopWidgets",
+                "BlueprintGraph",
+                "RHI",
+                "InterchangeEngine",
+                "Json",
+                "JsonUtilities",
+                "ClothingSystemEditorInterface",
+                "ClothingSystemRuntimeInterface",
+                "ClothingSystemRuntimeCommon",
+                "AnimGraphRuntime",
+                "CommonMenuExtensions",
+                "PinnedCommandList",
+                "RenderCore",
+                "SkeletalMeshUtilitiesCommon",
+                "NaniteUtilities",
+                "ToolMenus",
+                "CurveEditor",
+                "SequencerWidgets",
+                "TimeManagement",
+                "Sequencer",
+                "StatusBar",
+                "ToolWidgets",
+                "InteractiveToolsFramework",
+                "ContentBrowser",
+                "SkeletalMeshDescription",
+            }
+        );
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[] {
+                "Documentation",
+                "MainFrame",
+                "DesktopPlatform",
+                "SkeletonEditor",
+                "AssetTools",
+                "AnimationEditor",
+                "MeshReductionInterface",
+                "SequenceRecorder",
+            }
+        );
+    }
 }
